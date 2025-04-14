a1f952d7627d30c3d195ffc2ccbaeae84672cadb
rk-vinh <taint2@rikkeisoft.com>
2025-04-14 14:29:59 +0700

    test send mail 6

--- .github/scripts/send-email.mjs
+++ .github/scripts/send-email.mjs
@@ -14,15 +14,15 @@ const transporter = nodemailer.createTransport({
 
 (async () => {
   let branch = process.env.BRANCH_NAME;
-  let action = process.env.ACTION;
+  let actor = process.env.ACTOR;
   let email = 'nguyentantai13031999@gmail.com';
 
   try {
     await transporter.sendMail({
       from: '"Warning | DevOps" <nguyentantai13031999@gmail.com>',
       to: email,
-      subject: 'Stale Branch Detected!',
-      html: `Branch: <b>${branch}</b> have ${action} action!`,
+      subject: 'Test send mail from git action!',
+      html: `Branch: <b>${branch}</b> have ${actor} action!`,
     });
   } catch (error) {
     console.error(error);
